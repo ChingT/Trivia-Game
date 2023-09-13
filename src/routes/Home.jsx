7a68f9e58f5ecf6e0c49d111fcda78f949a5ceeb
ChingT <j60626j@gmail.com>
2023-09-13 23:07:01 +0200

    Fix useEffect

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,31 +16,29 @@ export default function Home() {
   const dispatch = useDispatch();
   const questions = useSelector((state) => state.questions.questions);
 
-  const fetchquestions = async (num = 10, questionType = "multiple") => {
-    try {
-      const res = await fetch(
-        `https://opentdb.com/api.php?amount=${num}&type=${questionType}`
-      );
-      const data = await res.json();
-      dispatch(loadQuestions(data.results));
-    } catch (e) {
-      console.log(e);
-      return redirect("/");
-    }
-  };
+  useEffect(() => {
+    const fetchquestions = async (num = 10) => {
+      try {
+        const res = await fetch(`https://opentdb.com/api.php?amount=${num}`);
+        const data = await res.json();
+        dispatch(loadQuestions(data.results));
+      } catch (e) {
+        console.log(e);
+        return redirect("/");
+      }
+    };
+
+    fetchquestions();
+  }, [dispatch]);
 
   useEffect(() => {
     if (!questions) return;
     Object.entries(questions).forEach(([questionNr, question]) => {
       const options = [question.correct_answer, ...question.incorrect_answers];
       options.shuffle();
-     dispatch( setOptions([questionNr, options]));
+      dispatch(setOptions([questionNr, options]));
     });
-  }, [questions]);
-
-  useEffect(() => {
-    fetchquestions();
-  }, []);
+  }, [dispatch, questions]);
 
   const navigate = useNavigate();
   const handleClick = () => {
@@ -50,7 +48,7 @@ export default function Home() {
   return (
     <main id="home">
       <h1>Welcome to Trivia Game</h1>
-      <button onClick={handleClick}>Start</button>
+      <button onClick={handleClick}>Start Quiz</button>
     </main>
   );
 }
