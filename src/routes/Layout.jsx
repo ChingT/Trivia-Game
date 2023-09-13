290a38a5448090e36cdfa2fbb6437fc4f3290574
ChingT <j60626j@gmail.com>
2023-09-13 16:05:34 +0200

    Remove Breadcrumb

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,12 +1,9 @@
 import Header from "../components/Header";
-import Breadcrumb from "../components/Breadcrumb";
 import { Outlet } from "react-router-dom";
-
 export default function Layout() {
   return (
     <>
       <Header />
-      <Breadcrumb />
       <Outlet />
     </>
   );
